e9249c324ee099d991f2f2f9b567a7e80300c6a2
TonyYin0418 <TonyYin@88.com>
2022-05-07 08:37:31 +0800

    Update Latex 代码

--- tex/code/1.cpp
+++ tex/code/1.cpp
@@ -6,25 +6,15 @@ int main()
 {
 	cin >> n >> k;
 	f[0][0][0] = 1;
-	for (int i = 1; i <= n; i++)
-	{
-		for (int S1 = 0; S1 < (1 << n); S1++)
-		{
-			if ((S1 & (S1 << 1)) == 0)
-			{
-				for (int S2 = 0; S2 < (1 << n); S2++)
+	for(int i = 1; i <= n; i++)
+	for(int S1 = 0; S1 < (1 << n); S1++)
+		if((S1 & (S1 << 1)) == 0)
+		for(int S2 = 0; S2 < (1 << n); S2++)
+			if(((S2 & (S2 << 1)) == 0) && (((S1 & S2) || (S1 & (S2 << 1)) || (S1 & (S2 >> 1))) == 0))
+				for(int j = 0; j <= k; j++)
 				{
-					if (((S2 & (S2 << 1)) == 0) && (((S1 & S2) || (S1 & (S2 << 1)) || (S1 & (S2 >> 1))) == 0))
-					{
-						for (int j = 0; j <= k; j++)
-						{
-							f[i][j + pc(S2)][S2] += f[i - 1][j][S1];
-						}
-					}
+					f[i][j + pc(S2)][S2] += f[i - 1][j][S1];
 				}
-			}
-		}
-	}
 	long long ans = 0;
 	for (int S = 0; S < (1 << n); S++)
 		ans += f[n][k][S];
